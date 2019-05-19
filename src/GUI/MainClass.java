6a94ef2cf359cef67b2acfed38ba4d5c0d6de83e
Omar Moghazy <omar.moghazy.42@gmail.com>
2019-05-19 23:29:44 +0200

    added gameplay fxml and controller

--- src/GUI/MainClass.java
+++ src/GUI/MainClass.java
@@ -1,15 +1,9 @@
 package GUI;
 
-import java.io.File;
-
-import gameObjects.SpecialFruit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class MainClass extends Application{
@@ -24,15 +18,6 @@ public class MainClass extends Application{
 		primaryStage.setScene(new Scene(root,713,563));
 		primaryStage.show();
 		
-		/*VBox borderpane = new VBox();
-		SpecialFruit x = new SpecialFruit();
-		Image image = x.getImages()[0];
-		ImageView imv = new ImageView();
-		imv.setImage(image);
-		borderpane.getChildren().add(imv);
-		Scene scene = new Scene(borderpane);
-		primaryStage.setScene(scene);
-		primaryStage.show();*/
 	}
 	
 
---
+++ src/GUI/MainMenuController.java
@@ -0,0 +1,79 @@
+package GUI;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class MainMenuController implements Initializable {
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+	@FXML private Button exitbutton;
+	@FXML private Button hardbutton;
+	@FXML private Button mediumbutton;
+	@FXML private Button easybutton;
+
+
+
+	@FXML
+	public void exitbutton(ActionEvent event) throws IOException{
+
+		System.exit(0);
+	}
+
+    @FXML
+	public void easybutton(ActionEvent event) throws IOException {
+		System.out.println("Easy");
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("GamePlay.fxml"));
+		Scene scene = new Scene(root, 1000, 552);
+		window.setScene(scene);
+		window.show();
+	}
+
+	@FXML
+	public void mediumbutton(ActionEvent event) throws IOException {
+		System.out.println("Medium");
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("GamePlay.fxml"));
+		Scene scene = new Scene(root, 1000, 552);
+		window.setScene(scene);
+		window.show();
+	}
+	@FXML
+	public void hardbutton(ActionEvent event) throws IOException {
+		System.out.println("Hard");
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Parent root = FXMLLoader.load(getClass().getResource("GamePlay.fxml"));
+		Scene scene = new Scene(root, 1000, 552);
+		window.setScene(scene);
+		window.show();
+	}
+	@FXML
+	public void randombutton(ActionEvent event) throws IOException  {
+		System.out.println("Random");
+		double x=Math.random();
+		if (x<=1/3)
+			hardbutton(event);
+		else if (x<=1/6)
+			mediumbutton(event);
+		else if (x<=1)
+			easybutton(event);
+	}
+
+
+}
