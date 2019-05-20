7b024fa79969c81a5c5673af2b120be025a57e89
Omar Moghazy <omar.moghazy.42@gmail.com>
2019-05-20 21:02:25 +0200

    working score and lives, moved some code over to GameActions

--- src/interfaces/IGameActions.java
+++ src/interfaces/IGameActions.java
@@ -1,25 +1,24 @@
 package interfaces;
 
 import gameObjects.GameObject;
+import javafx.scene.canvas.GraphicsContext;
 
 public interface IGameActions {
 	/*
 	 * @return created game object
 	 */
-	public GameObject createGameObject();
+	public void createGameObject();
 
 	/*
 	 * update moving objects locations
 	 */
-	public void updateObjectsLocations();
+	void updateObjectsLocations(GraphicsContext gc);
 	/*
 	* it is used to slice fruits located in your swiping region
 	This method can take your swiping region as parameters (they
 	depend on how you calculate it).
 	*/
-	public void sliceObjects();
-	/*
-	*resets the game to its initial state
-	*/
 	public void ResetGame();
+
+	
 }
